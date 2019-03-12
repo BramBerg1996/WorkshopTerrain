e10a2e977a1e506c43e4e433113403c9319acb13
StudentPegi <47355193+StudentPegi@users.noreply.github.com>
2019-03-12 15:56:27 +0100

    A.I.
    A.I. for the enemy

---
+++ TerrianProject/Assets/Pekka/Scripts/A.I_/ChaseState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : IEnemyState
+{
+    Enemy enemy;
+    Transform player;
+
+    public void OnEnter(Enemy enemy)
+    {
+        this.enemy = enemy;
+        player = GameObject.Find("Player").transform;
+        enemy.PlayerSpotted();
+
+        enemy.Invoke("ChasePlayer", 1f);
+    }
+
+    public void OnExit()
+    {
+        enemy.CancelInvoke();
+        enemy.chasePlayer = false;
+    }
+
+    public void UpdateState()
+    {
+        if (enemy.chasePlayer)
+        {
+            enemy.agent.destination = player.transform.position;
+        }
+    }
+
+
+}
---
+++ TerrianProject/Assets/Pekka/Scripts/A.I_/StateController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour
+{
+    public IEnemyState currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeState(new PatrolState());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.UpdateState();
+    }
+
+    public void ChangeState(IEnemyState playerState)
+    {
+        if (currentState != null)
+            currentState.OnExit();
+
+        currentState = playerState;
+
+        currentState.OnEnter(GetComponent<Enemy>());
+    }
+}
